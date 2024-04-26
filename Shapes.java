85291205b21b74bf6837e53ed41dcf8a0084ce4d
Francis-CyAI <kalungafrancis28@gmail.com>
2024-04-26 06:55:13 +0200

    Renamed file to shapes, and corrected auto-refactor errors.

--- Shapes.java
+++ Shapes.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner; // Scanner will be used to get user input and make the program interactive.
 
-public class Shape {
+public class Shapes {
     public static void main(String[] args) {
-        Shape[] shapes = new ArrayList<>(); // Keeps all shapes created during program execution.
+        ArrayList<Shape> shapes = new ArrayList<Shape>(); // Keeps all shapes created during program execution.
 
-        Scanner input = new Scanner(system.in);
+        Scanner input = new Scanner(System.in);
 
         do {
             // Run at least once, until user terminates program using sentinel value, 'q'.
