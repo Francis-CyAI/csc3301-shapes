3e342e97d8cc2f2123d9d8fceed105ebe7c1e4a3
Francis <79401136+Francis-CyAI@users.noreply.github.com>
2024-04-29 04:24:40 -0700

    Ready enough  for parsing.

--- Shapes.java
+++ Shapes.java
@@ -1,119 +1,191 @@
-import java.util.ArrayList;
 import java.util.Scanner; // Scanner will be used to get user input and make the program interactive.
 
 public class Shapes {
-    public static void main(String[] args) {
-        ArrayList<Shape> shapes = new ArrayList<Shape>(); // Keeps all shapes created during program execution.
+   static Scanner input = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.println("\nWelcome to our Shapes program! \nFeel free any time to exit by entering '-1'.");
+
 
         do {
             // Run at least once, until user terminates program using sentinel value, 'q'.
             // ...
-            break;
-        } while (true);
+            System.out.print("\nThe following shapes are available for creation: " +
+                    "\n\t1 - Rectangle, \n\t2 - Trapezium, \n\t3 - Rhombus, \n\t4 - Circle."
+                    + "\nEnter a number that corresponds to a shape to select that shape: ");
 
-        Shape rec = new Rectangle(10, 5);
-        Shape rhom = new Rhombus(5, 3, 2);
-        Shape trap = new Trapezium(5, 7, 6, 8, 6);
+            int ans = input.nextInt();
 
-        System.out.println(rec.getArea());
-        System.out.println(rec.getPerimeter());
-        System.out.println(rhom.getArea());
-        System.out.println(rhom.getPerimeter());
-        System.out.println(trap.getArea());
-        System.out.println(trap.getPerimeter());
+            Shape shape = null;
+
+            switch (ans) {
+                case 1:
+                    shape = new Rectangle(
+                            enter("length"),
+                            enter("width"));
+                    break;
+                case 2:
+                    shape = new Trapezium(
+                            enter("top length"),
+                            enter("bottom length"),
+                            enter("height"),
+                            enter("left side length"),
+                            enter("right side length"));
+                    break;
+                case 3:
+                    shape = new Rhombus(
+                            enter("side length"),
+                            enter("diagonal1 length"),
+                            enter("diagonal2 length"));
+                    break;
+                case 4:
+                    shape = new Circle(
+                            enter("radius"));
+                    break;
+                case -1:
+                    enter("-1");
+                default:
+                    continue;
+            }
+            if (!shape.equals(null)) {
+                System.out.println("This " + shape.name + " has a perimeter of "
+                        + shape.getPerimeter() + " and  an area of " + shape.getArea() + ".");
+            }
+        } while (true);        
+    }
+
+    static double enter(String w) {
+        if (w.equals("-1")) {
+            close();
+        }
+        System.out.print("\n\tEnter " + w + ": ");
+        double in = input.nextDouble();
+        if (in < 0) {
+           close();
+        }
+        return in;
     }
     
+    static void close() {
+        System.out.print("Good bye!");
+        System.exit(1);
+    }
 }
-
 
 abstract class Shape {
     /**
      * This is the base shape class from which all other shape classes will inherit.
-     * @author Francis Kalunga
+     * 
+     * @Author Francis Kalunga
      */
+    public String name;
+
     public abstract double getPerimeter();
 
     public abstract double getArea();
 }
 
-abstract class Polygon extends Shape {
+class Circle extends Shape {
     /**
-     * @Author Francis Kalunga
+     * @author Francis Kalunga
      */
-    double[] sides; // = new double[] {12.5, 14, ...};
-    // private double l = sides[0]; // length
-    // private double h = sides[1]; // height
+
+    private double r;
+
+    public Circle(double radius) {
+        r = radius;
+        super.name = "Circle";
+    }
 
     @Override
     public double getPerimeter() {
-        double p = 0;
-        for (int i = 0; i < sides.length; i++) {
-            p += sides[i];
-        }
-        return p;
+        return 2 * 3.1416 * r; 
     }
-}
-
-abstract class Quadrilateral extends Polygon {
-    /**
-     * @Author Mike Shatamuka
-     */
-    public Quadrilateral(double side1, double side2, double side3, double side4) {
-        sides = new double[] { side1, side2, side3, side4 };
-    }
-
-}
-
-abstract class Octagon extends Polygon {
-    /**
-     * @Author Mike Shatamuka
-     */
-    public Octagon(double sideLength) {
-        sides = new double[8];
-        for (int i = 0; i < 8; i++) {
-            sides[i] = sideLength;
-        }
-    }
-
-}
-
-class Rectangle extends Polygon {
-    public Rectangle(double length, double width) {
-        super.sides = new double[]{length, width};
-    }
-
     @Override
     public double getArea() {
-        return super.sides[0] * super.sides[1];
+        return 3.1416 * r * r;
+    }
+}    
+    abstract class Polygon extends Shape {
+        /**
+         * @Author Francis Kalunga
+         */
+        double[] sides; // = new double[] {12.5, 14, ...};
+        // private double l = sides[0]; // length
+        // private double h = sides[1]; // height
+
+        @Override
+        public double getPerimeter() {
+            double p = 0;
+            for (int i = 0; i < sides.length; i++) {
+                p += sides[i];
+            }
+            return p;
+        }
     }
 
-    @Override
-    public double getPerimeter() {
-        return 2 * (super.sides[0] + super.sides[1]);
-    }
-}
+    abstract class Quadrilateral extends Polygon {
+        /**
+         * @Author Mike Shatamuka
+         */
+        public Quadrilateral(double side1, double side2, double side3, double side4) {
+            sides = new double[] { side1, side2, side3, side4 };
+        }
 
-class Trapezium extends Polygon {
-    public Trapezium(double a, double b, double h, double c, double d) {
-        super.sides = new double[] { a, b, h, c, d };
-    }
-
-    @Override
-    public double getArea() {
-        return sides[2] * (sides[0] + sides[1]) / 2;
     }
 
-    @Override
-    public double getPerimeter() {
-        return super.getPerimeter() - sides[2]; // minus h
+    abstract class Octagon extends Polygon {
+        /**
+         * @Author Mike Shatamuka
+         */
+        public Octagon(double sideLength) {
+            sides = new double[8];
+            for (int i = 0; i < 8; i++) {
+                sides[i] = sideLength;
+            }
+        }
+
     }
-}
+
+    class Rectangle extends Polygon {
+        public Rectangle(double length, double width) {
+            super.sides = new double[] { length, width };
+            super.name = "Rectangle";
+        }
+
+        @Override
+        public double getArea() {
+            return super.sides[0] * super.sides[1];
+        }
+
+        @Override
+        public double getPerimeter() {
+            return 2 * (super.sides[0] + super.sides[1]);
+        }
+    }
+
+    class Trapezium extends Polygon {
+        public Trapezium(double a, double b, double h, double c, double d) {
+            super.sides = new double[] { a, b, h, c, d };
+            super.name = "Trapezium";
+        }
+
+        @Override
+        public double getArea() {
+            return sides[2] * (sides[0] + sides[1]) / 2;
+        }
+
+        @Override
+        public double getPerimeter() {
+            return super.getPerimeter() - sides[2]; // minus h
+        }
+    }
 
 class Rhombus extends Polygon {
     public Rhombus(double side, double diagonal1, double diagonal2) {
-        super.sides =  new double[] { side, diagonal1, diagonal2, side };
+        super.sides = new double[] { side, diagonal1, diagonal2, side };
+        super.name = "Rhombus";
     }
 
     @Override
@@ -121,4 +193,3 @@ class Rhombus extends Polygon {
         return sides[1] * sides[2] / 2;
     }
 }
-
